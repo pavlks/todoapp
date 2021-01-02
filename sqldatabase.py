02e27781fcd7e6403e3b40b4ac921c630f0c2626
Pavel Sazonov <sazonov.ps@protonmail.com>
2021-01-02 15:01:37 -0500

    refactored function show_today v4.0

--- sqldatabase.py
+++ sqldatabase.py
@@ -67,8 +67,8 @@ class SQLdatabase:
     def clear_today(self):
         logging.info("  " + str(datetime.datetime.now()) + "  " + ">" * 20 + "     " + "CLEARING TODAY TODOS" + "     " + "<" * 20)
         stmt = self.todos.update().\
-                    where(self.todos.c.is_today == True).\
-                    values(name=False)
+                    where(self.todos.c.is_today == True).where(self.todos.c.completed == None).\
+                    values(description=False)
         connection = self.engine.connect()
         res = connection.execute(stmt)
         return True
