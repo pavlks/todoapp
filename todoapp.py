2afd24f268a3e69e469941a3257ec6121053ed57
Pavel Sazonov <sazonov.ps@protonmail.com>
2021-01-02 02:10:19 -0500

    refactored to use SQLite db ver 6.6

--- todoapp.py
+++ todoapp.py
@@ -173,7 +173,8 @@ def telegram_webhook():
 
         else:
             todo = Todo.process_input(message)
-            db.add_record(todo.description, todo.is_today, todo.notify_date, todo.notify_time, todo.category)
+            db.add_record(todo.description)
+            #  db.add_record(todo.description, todo.is_today, todo.notify_date, todo.notify_time, todo.category)
             payload = {
                     'chat_id': chat_id,
                     'text': '<b>new to-do was added</b>',
